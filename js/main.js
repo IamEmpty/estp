958b1c068cfc90385cb51e18d239614874f47739
IamEmpty <dustonroad@gmail.com>
2014-06-24 16:38:18 +0400

    add new view for tables and forms

--- js/main.js
+++ js/main.js
@@ -2,9 +2,4 @@ $(document).ready(function() {
     $('.panel-group').accordionWithLinks({
         animationDuration: 400
     });
-
-    $('.panel-group2').accordionWithLinks({
-        animationDuration: 2000,
-        accordionHeading: '.panel-heading'
-    });
 });
